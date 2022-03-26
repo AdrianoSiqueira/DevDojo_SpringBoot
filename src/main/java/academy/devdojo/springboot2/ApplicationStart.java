d28481c42868c853616dde3d4fb122a24d8f8c5b
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2022-03-26 19:22:43 -0300

    Implementação da anotação @SpringBootApplication que engloba as anotações necessárias.

--- src/main/java/academy/devdojo/springboot2/ApplicationStart.java
+++ src/main/java/academy/devdojo/springboot2/ApplicationStart.java
@@ -1,11 +1,9 @@
 package academy.devdojo.springboot2;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class ApplicationStart {
 
     public static void main(String[] args) {
