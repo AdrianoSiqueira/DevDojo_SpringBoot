13baf693ecbe69461c443f022e0f87831b41e84d
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2022-03-26 18:49:17 -0300

    Adição dos mapeamentos do endpoint.

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -1,14 +1,18 @@
 package academy.devdojo.springboot2.controller;
 
 import academy.devdojo.springboot2.domain.Anime;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping(path = "anime")
 public class AnimeController {
 
-    public List<Anime> list(){
+    @GetMapping(path = "list")
+    public List<Anime> list() {
         return List.of(
                 new Anime("DBZ"),
                 new Anime("Berserk")
