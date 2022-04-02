57cb117e6b69c0c5252961ad9cf25ca059d9f622
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2022-04-02 12:13:06 -0300

    Adequar à convenção.

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "anime")
+@RequestMapping(path = "animes")
 @Log4j2
 @RequiredArgsConstructor
 public class AnimeController {
