8f9db9c3df65da20629325ae7c8e0cc4a40689d9
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2022-04-02 13:28:52 -0300

    Implementar pesquisa por id.

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,10 +23,14 @@ public class AnimeController {
     private final DateUtil     dateUtil;
     private final AnimeService animeService;
 
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable long id) {
+        return ResponseEntity.ok(animeService.findById(id));
+    }
+
     @GetMapping
     public ResponseEntity<List<Anime>> list() {
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-
         return ResponseEntity.ok(animeService.listAll());
     }
 }
