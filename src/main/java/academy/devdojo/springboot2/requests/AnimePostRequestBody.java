103d227b368b02700bb7eb102307ebb4784fae76
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2022-04-09 20:38:43 -0300

    Remover anotação desnecessária.

--- src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
@@ -3,11 +3,10 @@ package academy.devdojo.springboot2.requests;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 @Data
 public class AnimePostRequestBody {
-    @NotNull(message = "Anime name cannot be null")
-    @NotEmpty(message = "Anime name cannot be empty")
+
+    @NotEmpty(message = "Anime name cannot be empty nor null")
     private String name;
 }
