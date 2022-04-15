9f74c70b4a709377602567c30c44bc867fb5f3f4
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2022-04-15 14:08:52 -0300

    Adicionar método para retornar lista de animes sem paginação.

--- src/main/java/academy/devdojo/springboot2/client/SpringClient.java
+++ src/main/java/academy/devdojo/springboot2/client/SpringClient.java
@@ -2,8 +2,13 @@ package academy.devdojo.springboot2.client;
 
 import academy.devdojo.springboot2.domain.Anime;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Essa classe simula um cliente se conectando com nosso servidor. Para ela
@@ -13,13 +18,25 @@ import org.springframework.web.client.RestTemplate;
 public class SpringClient {
 
     public static void main(String[] args) {
-        String url = "http://localhost:8080/animes/200";
+        String getById = "http://localhost:8080/animes/2";
+        String getAll  = "http://localhost:8080/animes/all";
 
         // O jackson irá converter o json para Anime automaticamente
-        ResponseEntity<Anime> response = new RestTemplate().getForEntity(url, Anime.class);
+        ResponseEntity<Anime> response = new RestTemplate().getForEntity(getById, Anime.class);
         log.info(response);
 
-        Anime anime = new RestTemplate().getForObject(url, Anime.class);
+        Anime anime = new RestTemplate().getForObject(getById, Anime.class);
         log.info(anime);
+
+        Anime[] animesArray = new RestTemplate().getForObject(getAll, Anime[].class);
+        log.info(Arrays.toString(animesArray));
+
+        ResponseEntity<List<Anime>> animeListResponse = new RestTemplate().exchange(
+                getAll,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {}
+        );
+        log.info(animeListResponse.getBody());  // getBody retorna a lista
     }
 }
