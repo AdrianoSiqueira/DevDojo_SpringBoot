04ddecbe74e97a846ca8051595fdd7a90a6ef12d
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2022-04-16 21:14:17 -0300

    Adicionar classe de testes para o AnimeRepository.

---
+++ src/test/java/academy/devdojo/springboot2/repository/AnimeRepositoryTest.java
@@ -0,0 +1,31 @@
+package academy.devdojo.springboot2.repository;
+
+import academy.devdojo.springboot2.domain.Anime;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class AnimeRepositoryTest {
+
+    @Autowired
+    private AnimeRepository animeRepository;
+
+    @Test
+    void test() {
+        Anime animeToSave = createAnime();
+        Anime savedAnime  = animeRepository.save(animeToSave);
+
+        assertThat(savedAnime).isNotNull();
+        assertThat(savedAnime.getId()).isNotNull();
+        assertThat(savedAnime.getName()).isEqualTo(animeToSave.getName());
+    }
+
+    private Anime createAnime() {
+        return Anime.builder()
+                    .name("Test")
+                    .build();
+    }
+}
