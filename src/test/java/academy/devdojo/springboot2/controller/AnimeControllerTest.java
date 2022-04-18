5e003fe4092673dcb4d348760290abdf55c1235f
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2022-04-18 17:01:03 -0300

    Adicionar classe de teste para o AnimeController.

---
+++ src/test/java/academy/devdojo/springboot2/controller/AnimeControllerTest.java
@@ -0,0 +1,73 @@
+package academy.devdojo.springboot2.controller;
+
+import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.service.AnimeService;
+import academy.devdojo.springboot2.util.AnimeCreator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/*
+ * @ExtendWith é usado para não carregar totalmente o spring, isso fará os testes
+ * serem executados mais rápido.
+ */
+@ExtendWith(value = SpringExtension.class)
+class AnimeControllerTest {
+
+    /*
+     * @InjectMocks é usado para testar a classe em si (no caso AnimeController)
+     */
+    @InjectMocks
+    private AnimeController animeController;
+
+    /*
+     * @Mock é usado para testar as classes dentro da classe que está sendo
+     * testada.
+     *
+     * Como a classe AnimeController faz uso da classe AnimeService, vamos
+     * declará-la como mock. Esse AnimeService irá se sobrepor sobre o que foi
+     * declarado na classe AnimeController.
+     *
+     * Vamos usar esse AnimeService para interceptar as chamadas a seus métodos
+     * para alterar o comportamento durante os testes.
+     */
+    @Mock
+    private AnimeService animeServiceMock;
+
+    @Test
+    void list_ReturnsListOfAnimesInsidePageObject_WhenSuccessful() {
+        String      expectedName = AnimeCreator.createValidAnime().getName();
+        Page<Anime> page         = animeController.list(null).getBody();
+
+        assertThat(page).isNotNull();
+
+        assertThat(page.toList())
+                .isNotEmpty()
+                .hasSize(1);
+
+        assertThat(page.toList().get(0).getName())
+                .isEqualTo(expectedName);
+    }
+
+    /*
+     * Intercepta as chamadas ao animeServiceMock e modifica o valor de retorno.
+     */
+    @BeforeEach
+    void setUp() {
+        PageImpl<Anime> animePage = new PageImpl<>(List.of(AnimeCreator.createValidAnime()));
+
+        BDDMockito.when(animeServiceMock.listAll(ArgumentMatchers.any()))
+                  .thenReturn(animePage);
+    }
+}
