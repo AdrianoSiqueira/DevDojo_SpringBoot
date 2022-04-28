6d9bbf60eae8f3b37d7456d67a3974dd04e94c10
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2022-04-28 19:47:46 -0300

    Adicionar anotação builder do lombok.

--- src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
@@ -1,10 +1,12 @@
 package academy.devdojo.springboot2.requests;
 
+import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
 
 @Data
+@Builder
 public class AnimePostRequestBody {
 
     @NotEmpty(message = "Anime name cannot be empty nor null")
