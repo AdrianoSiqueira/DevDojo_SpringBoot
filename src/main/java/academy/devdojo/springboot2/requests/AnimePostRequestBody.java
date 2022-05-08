d2cc5488a4b9ad3a0728e64f61d8dec3f7ce4fc8
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2022-05-07 22:20:23 -0300

    Adicionar construtores vazio e completo.

--- src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
@@ -1,12 +1,16 @@
 package academy.devdojo.springboot2.requests;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AnimePostRequestBody {
 
     @NotEmpty(message = "Anime name cannot be empty nor null")
