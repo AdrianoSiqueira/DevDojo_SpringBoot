c15c6392ca8ec8a5f9dd54b6731539dff65ac6e4
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2022-05-09 19:35:56 -0300

    Adicionar classe de configuração de autenticação.

---
+++ src/main/java/academy/devdojo/springboot2/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package academy.devdojo.springboot2.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * Esse método irá criar um usuário em memória com a senha criptografada.
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+            .withUser("Adriano")
+            .password(encoder.encode("123"))
+            .roles("USER")
+            .and()
+            .withUser("Devdojo")
+            .password(encoder.encode("academy"))
+            .roles("USER", "ADMIN");
+    }
+
+    /**
+     * Sobrescrevemos esse método para informar que todas as requisições ao
+     * servidor deverão passar por uma autenticação básica via http.
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .httpBasic();
+    }
+}
