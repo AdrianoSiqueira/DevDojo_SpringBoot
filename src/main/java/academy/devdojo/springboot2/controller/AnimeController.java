9f8e627d6258df663c195a7d7617842d6de6bc8f
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2022-05-10 20:37:38 -0300

    Adicionar pré autorização para buscar anime por id.

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -46,6 +46,7 @@ public class AnimeController {
     }
 
     @GetMapping(path = "/by-id/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Anime> findById(@PathVariable long id, @AuthenticationPrincipal UserDetails userDetails) {
         return ResponseEntity.ok(animeService.findByIdOrThrowBadRequestException(id));
     }
